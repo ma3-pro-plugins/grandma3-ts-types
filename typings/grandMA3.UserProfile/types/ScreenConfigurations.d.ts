80af41edfdab1e7db6328006df2986a77a51213c
Erez Makavy <hepi.yellow@gmail.com>
2022-12-13 14:53:23 +0200

    Add ScreenConfigurations

---
+++ typings/grandMA3.UserProfile/types/ScreenConfigurations.d.ts
@@ -0,0 +1,28 @@
+type ScreenConfigurations = Obj<UserProfile, ScreenConfiguration> &
+    ScreenConfiguration[] & { [index: string]: ScreenConfiguration };
+
+type ScreenConfiguration = Obj<ScreenConfigurations, ScreenContents>
+
+type ScreenContents = Obj<ScreenConfiguration, ScreenContent>
+
+type ScreenContent = Obj<ScreenContents, WindowBase>
+
+interface WindowBase extends Obj<ScreenContent, any> {
+    WindowAppearance: WindowAppearance,
+    WindowScrollPositions: WindowScrollPositions
+}
+type WindowAppearance = Obj<WindowBase, null>
+type WindowScrollPositions = Obj<WindowBase, null>
+
+interface WindowLayoutView extends WindowBase {
+    name: 'WindowLayoutView'
+    LayoutViewSettings: LayoutViewSettings
+}
+
+interface LayoutViewSettingsProps {
+    Layout: Layout,
+    FitType: 'Elements' | 'Canvas' | 'Both'
+    ShowTitleBar: boolean
+    AutoFit: boolean
+}
+type LayoutViewSettings = Obj<WindowBase, null> & LayoutViewSettingsProps
--- typings/grandMA3.UserProfile/types/index.d.ts
+++ typings/grandMA3.UserProfile/types/index.d.ts
@@ -1,1 +1,2 @@
 import './LayoutElementDefaultsCollect';
+import './ScreenConfigurations';
