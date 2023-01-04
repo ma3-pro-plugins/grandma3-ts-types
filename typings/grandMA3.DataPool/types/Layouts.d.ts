c4a4df8ab8cb0b5232e15749c73d4bd7ab594e82
Erez Makavy <hepi.yellow@gmail.com>
2023-01-04 13:43:46 +0200

    Layout props

--- typings/grandMA3.DataPool/types/Layouts.d.ts
+++ typings/grandMA3.DataPool/types/Layouts.d.ts
@@ -4,8 +4,34 @@ type Layout = Obj<Layouts, Element> & Element[] & { [index: string]: Element };
 
 type Element = Obj<Layout, null> &
 	null[] & { [index: string]: null } & {
-		AssignType: string;
-		Action: number;
-		Appearance: Appearance;
-		VisibilityObjectName: string;
+		id: number
+		assignType: number
+		action: number
+		appearance: Appearance
+		borderSize: number
+		/** 8 character hex string RGBA */
+		borderColor: string
+		customText: string
+		fontSize: 'Default' | 10 | 12 | 14 | 16 | 18 | 20 | 24 | 28 | 32
+		h: number
+		visibilityElement: boolean
+		visibilityBar: boolean
+		visibilityObjectName: boolean
+		visibilityID: boolean
+		visibilityCID: boolean
+		visibilityBorder: boolean
+		visibilityValue: boolean
+		visibilityIndicatorBar: boolean
+		visibilitySelectionRelevance: boolean
+		paddingLeft: number
+		paddingRight: number
+		paddingTop: number
+		paddingBottom: number
+		posX: number
+		posY: number
+		textAlignmentH: 'Center' | 'Left' | 'Right'
+		textAlignmentV: 'Center' | 'Top' | 'Bottom' | 'Above'
+		/** 8 character hex string RGBA */
+		textColor: string
+		w: number
 	};
