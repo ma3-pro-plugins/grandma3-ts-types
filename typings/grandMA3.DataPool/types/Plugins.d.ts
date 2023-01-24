62abf198c6fc2e3e664371a940ca3164cc493f75
Erez Makavy <hepi.yellow@gmail.com>
2023-01-24 12:17:01 +0200

    More lfs functions

--- typings/grandMA3.DataPool/types/Plugins.d.ts
+++ typings/grandMA3.DataPool/types/Plugins.d.ts
@@ -1,6 +1,22 @@
 type Plugins = Obj<DataPool, Plugin> & (Plugin | null)[] & { [index: string]: Plugin | null };
 
-type Plugin = Obj<Plugins, LuaComponent> &
+type PluginProps = {
+	scribble: Obj<any, any>
+	appearance: Obj<any, any>
+	author: string
+	version: string
+	path: string
+	userRights: string
+}
+type Plugin = Obj<Plugins, LuaComponent, PluginProps> &
 	(LuaComponent | null)[] & { [index: string]: LuaComponent | null };
 
-type LuaComponent = Obj<Plugin, null> & null[] & { [index: string]: null };
+type LuaComponentProps = {
+	fileName: string
+	filePath: string
+	fileSize: number
+	isResource: boolean
+	inStream: boolean
+	installed: boolean
+}
+type LuaComponent = Obj<Plugin, null, LuaComponentProps> & null[] & { [index: string]: null };
