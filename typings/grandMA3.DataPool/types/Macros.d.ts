686a620f21864ba4e8f9d1869b5a29e7463b6748
Erez Makavy <hepi.yellow@gmail.com>
2023-05-04 15:14:20 +0300

    Add MacroLineProps

--- typings/grandMA3.DataPool/types/Macros.d.ts
+++ typings/grandMA3.DataPool/types/Macros.d.ts
@@ -2,5 +2,12 @@ type Macros = Obj<DataPool, Macro> & Macro[] & { [index: string]: Macro };
 
 type Macro = Obj<Layouts, MacroLine> & MacroLine[] & { [index: string]: MacroLine };
 
-type MacroLineProps = ObjProps & {};
+type MacroLineProps = ObjProps & {
+    wait: number
+    command: string
+    note: string
+    enabled: boolean
+    addToCmdLine: boolean
+    execute: boolean
+};
 type MacroLine = Obj<Macro, undefined> & MacroLineProps;
