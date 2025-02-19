2afe49e8d857ae6ff0c26ab1192ba11955ae25aa
Erez Makavy <hepi.yellow@gmail.com>
2025-02-19 22:31:27 +0200

    USB Notifider

--- typings/grandMA3.Root/Root.d.ts
+++ typings/grandMA3.Root/Root.d.ts
@@ -3,4 +3,17 @@ type Root = Obj<undefined, any> & { [index: string]: any } & {
 	ColorTheme: ColorTheme;
 	DeviceConfigurations: DeviceConfigurations;
 	GraphicsRoot: GraphicsRoot;
+	UsbNotifier: UsbNotifier;
 };
+
+type UsbNotifier = Obj<Root, StorageDevice> & {
+	StorageDevice: StorageDevice;
+}
+type StorageDevice = Obj<UsbNotifier, USBDeviceStorage> & USBDeviceStorage[]
+type USBDeviceStorageProps = ObjProps & {
+	connected: boolean;
+	connectedCount: any;
+	ip: any;
+	mountPoint: any;
+}
+type USBDeviceStorage = Obj<USBDeviceStorage, void,USBDeviceStorageProps, 'USBDeviceStorage'> 
