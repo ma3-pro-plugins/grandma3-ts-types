e8125fdb8bfc02334e0226af113abc830fd293be
Erez Makavy <hepi.yellow@gmail.com>
2025-10-23 08:48:23 +0300

    MANetSocketProps

--- typings/grandMA3.Root/Root.d.ts
+++ typings/grandMA3.Root/Root.d.ts
@@ -3,17 +3,49 @@ type Root = Obj<undefined, any> & { [index: string]: any } & {
 	ColorTheme: ColorTheme;
 	DeviceConfigurations: DeviceConfigurations;
 	GraphicsRoot: GraphicsRoot;
+	MANetSocket: MANetSocket;
 	UsbNotifier: UsbNotifier;
+	Temp: RootTemp;
 };
 
 type UsbNotifier = Obj<Root, StorageDevice> & {
 	StorageDevice: StorageDevice;
-}
-type StorageDevice = Obj<UsbNotifier, USBDeviceStorage> & USBDeviceStorage[]
+};
+type StorageDevice = Obj<UsbNotifier, USBDeviceStorage> & USBDeviceStorage[];
 type USBDeviceStorageProps = ObjProps & {
 	connected: boolean;
 	connectedCount: any;
 	ip: any;
 	mountPoint: any;
-}
-type USBDeviceStorage = Obj<USBDeviceStorage, void,USBDeviceStorageProps, 'USBDeviceStorage'> 
+};
+type USBDeviceStorage = Obj<USBDeviceStorage, void, USBDeviceStorageProps, 'USBDeviceStorage'>;
+
+type RootTemp = Obj<Root, any> & {
+	DriveCollect: DriveCollect;
+};
+
+type DriveCollect = Obj<RootTemp, Drive>;
+type Drive = Obj<DriveCollect, void, DriveProps, 'Drive'> & DriveProps;
+type DriveProps = ObjProps & {
+	driveType: 'Removeable' | 'Internal' | 'OldVersion';
+	path: string;
+};
+
+type MANetSocket = Obj<Root, void, MANetSocketProps, 'MAnetSocket'> & MANetSocketProps;
+type MANetSocketProps = ObjProps & {
+	hostName: string;
+	/**
+	 * Readonly
+	 */
+	primaryIp: string;
+	session: string;
+	showfile: string;
+	/**
+	 * Readonly
+	 */
+	status: 'IdleMaster' | string;
+	/**
+	 * Readonly
+	 */
+	sessionManager: 'Yes' | 'No';
+};
