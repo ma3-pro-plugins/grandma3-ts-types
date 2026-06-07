7ae26df9dc0a76ae1ea56f59a6b58a36d9c0c28a
Erez Makavy <hepi.yellow@gmail.com>
2026-06-07 21:08:20 +0300

    v2.4 changes in UserProfile

--- typings/grandMA3.UserProfile/functions.d.ts
+++ typings/grandMA3.UserProfile/functions.d.ts
@@ -6,13 +6,69 @@ type UserProfileProps = ObjProps & {
 type UserProfile = Obj<UserProfiles, any, UserProfileProps> &
 	any[] & { [index: string]: any } & {
 		Environments: Environments;
+		EncoderBarPool: EncoderBarPool;
+		Cameras: Cameras;
 		Views: Views;
-		KeyboardShortCuts: KeyboardShortCuts;
+		StorePreferences: StorePreferences;
+		ExecutorFixation: ExecutorFixation;
+		SpecialExecutorPages: SpecialExecutorPages;
+		TemporaryWindowSettings: TemporaryWindowSettings;
+		SmartViewPool: SmartViewPool;
+		Variables: UserProfileVariables;
 		ScreenConfigurations: ScreenConfigurations;
 		LayoutElementDefaultsCollect: LayoutElementDefaultsCollect;
+		KeyboardShortCuts: KeyboardShortCuts;
 		UserAttributePreferences: UserAttributePreferences;
+		RenderQualities: RenderQualities;
+		GridColumnRegistry: GridColumnRegistry;
+		StatusCenter: StatusCenter;
+		PreviewSettings: PreviewSettings;
+		MessageNotificationSettings: MessageNotificationSettings;
 		Name: string;
 	};
+
+declare namespace MA3_V2_3 {
+	type UserProfile = Obj<UserProfiles, any, UserProfileProps> &
+		any[] & { [index: string]: any } & {
+			Environments: Environments;
+			EncoderBarPool: EncoderBarPool;
+			Cameras: Cameras;
+			WindowTypes: WindowTypes;
+			Views: Views;
+			StorePreferences: StorePreferences;
+			ExecutorFixation: ExecutorFixation;
+			SequenceSelection: SequenceSelection;
+			SpecialExecutorPages: SpecialExecutorPages;
+			TemporaryWindowSettings: TemporaryWindowSettings;
+			SmartViewPool: SmartViewPool;
+			Variables: UserProfileVariables;
+			ScreenConfigurations: ScreenConfigurations;
+			LayoutElementDefaultsCollect: LayoutElementDefaultsCollect;
+			KeyboardShortCuts: KeyboardShortCuts;
+			UserAttributePreferences: UserAttributePreferences;
+			RenderQualities: RenderQualities;
+			Collection: Collection;
+			GridColumnRegistry: GridColumnRegistry;
+			StatusCenter: StatusCenter;
+			Name: string;
+		};
+}
+
+type EncoderBarPool = Obj<UserProfile, any>;
+type Cameras = Obj<UserProfile, any>;
+type StorePreferences = Obj<UserProfile, any>;
+type ExecutorFixation = Obj<UserProfile, any>;
+type SpecialExecutorPages = Obj<UserProfile, any>;
+type TemporaryWindowSettings = Obj<UserProfile, any>;
+type SmartViewPool = Obj<UserProfile, any>;
+type UserProfileVariables = Obj<UserProfile, any>;
+type RenderQualities = Obj<UserProfile, any>;
+type GridColumnRegistry = Obj<UserProfile, any>;
+type StatusCenter = Obj<UserProfile, any>;
+type PreviewSettings = Obj<UserProfile, any>;
+type MessageNotificationSettings = Obj<UserProfile, any>;
+type WindowTypes = Obj<MA3_V2_3.UserProfile, any>;
+type Collection = Obj<MA3_V2_3.UserProfile, any>;
 
 type Environments = Obj<Environments, UserEnvironment> & {
 	/**
